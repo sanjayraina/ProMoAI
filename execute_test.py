f7e82b7e383e9278698979a51001cbdf479b2a2c
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-12-12 07:17:08 +0100

    revision

--- execute_test.py
+++ execute_test.py
@@ -6,9 +6,11 @@ import json
 import random
 import traceback
 
-model = "Qwen/QwQ-32B-Preview"
-api_key = "ZFwz90wwIzRwPcrTGbecxSq6SjOUClrv"
-api_url = "https://api.deepinfra.com/v1/openai"
+
+model = "o1-2024-12-05"
+api_key = "XXXXX"
+api_url = "https://api.openai.com/v1"
+manual = True
 print("== starting")
 
 gt_pro_desc_folder = r"C:\Users\berti\EvaluatingLLMsProcessModeling\ground_truth\ground_truth_process_descriptions\long"
@@ -17,7 +19,7 @@ gt_pn_folder = r"C:\Users\berti\EvaluatingLLMsProcessModeling\ground_truth\groun
 fold1_content = os.listdir(gt_pro_desc_folder)
 fold2_content = os.listdir(gt_pn_folder)
 
-root_folder = r"C:\Users\berti\EvaluatingLLMsProcessModeling\results_benchmarking\QwQ-32B-Preview"
+root_folder = r"C:\Users\berti\EvaluatingLLMsProcessModeling\results_benchmarking\o1-2024-12-05"
 code_folder = os.path.join(root_folder, "code")
 conv_folder = os.path.join(root_folder, "conv")
 pn_folder = os.path.join(root_folder, "pn")
@@ -48,25 +50,53 @@ for idx in range(len(fold1_content)):
             activities = [x.label for x in net0.transitions if x.label is not None]
             random.shuffle(activities)
 
-
             prompt = pro_desc + "\n\nEnsure the generated model uses the following activity labels (please also note upper and lower case): "+", ".join(activities)
 
-            gen = LLMProcessModelGenerator(process_description=prompt, api_key=api_key, api_url=api_url,
-                                           openai_model=model)
+            if manual:
+                import pyperclip
+                import subprocess
+                from utils.prompting.prompt_engineering import create_model_generation_prompt
+                from utils.model_generation import code_extraction
+                prompt = create_model_generation_prompt(prompt)
+                while True:
+                    pyperclip.copy(prompt)
+                    F = open(target_code, "w")
+                    F.close()
+                    subprocess.run(["notepad.exe", target_code])
+                    F = open(target_code, "r")
+                    code = F.read()
+                    F.close()
+                    try:
+                        final_model = None
+                        final_model = code_extraction.execute_code_and_get_variable(code, "final_model")
+                        print(final_model)
+                        conv = []
+                        F = open(target_conv, "w")
+                        json.dump(conv, F, indent=2)
+                        F.close()
 
-            conv = gen.get_conversation()
-            F = open(target_conv, "w")
-            json.dump(conv, F, indent=2)
-            F.close()
+                        net, im, fm = pm4py.convert_to_petri_net(final_model)
+                        pm4py.write_pnml(net, im, fm, target_pn)
+                        break
+                    except:
+                        traceback.print_exc()
 
-            code = gen.get_code()
-            F = open(target_code, "w")
-            F.write(str(code))
-            F.close()
+            else:
+                gen = LLMProcessModelGenerator(process_description=prompt, api_key=api_key, api_url=api_url,
+                                               openai_model=model)
+                conv = gen.get_conversation()
+                F = open(target_conv, "w")
+                json.dump(conv, F, indent=2)
+                F.close()
 
-            powl = gen.get_powl()
-            net, im, fm = pm4py.convert_to_petri_net(powl)
-            pm4py.write_pnml(net, im, fm, target_pn)
+                code = gen.get_code()
+                F = open(target_code, "w")
+                F.write(str(code))
+                F.close()
+
+                powl = gen.get_powl()
+                net, im, fm = pm4py.convert_to_petri_net(powl)
+                pm4py.write_pnml(net, im, fm, target_pn)
         except:
             traceback.print_exc()
             continue
